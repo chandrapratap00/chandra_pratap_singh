7feea19e0b9b20bd204aec2d07a00be6a2119e82
chandrapratap00 <122519706+chandrapratap00@users.noreply.github.com>
2023-05-16 16:49:39 +0530

    Add files via upload

---
+++ chandra pratap python proggramme'/Python program to read an entire text file 48.py
@@ -0,0 +1,7 @@
+file_path = input("Enter the file path: ")
+
+with open(file_path, 'r') as file:
+    file_contents = file.read()
+
+print("File Contents:")
+print(file_contents)
---
+++ chandra pratap python proggramme'/Python program to reverse words in a given string 54.py
@@ -0,0 +1,9 @@
+
+def reverseWords(string):
+    words = string.split()
+    reversed_words = ' '.join(reversed(words))
+    return reversed_words
+
+string = input("Enter a string: ")
+reversed_string = reverseWords(string)
+print("Reversed string:", reversed_string)
